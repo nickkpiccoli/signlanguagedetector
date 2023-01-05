d8fef0a0bc7060e9791a469e56544a662995b54b
nickkpiccoli <piccolibeatz@gmail.com>
2023-01-05 19:41:43 +0100

    Created Main file
    main file

--- main.py
+++ main.py
@@ -1,60 +1,5 @@
 import cv2
-import mediapipe as mp
-
-class CapturedHand:
-    def __init__(self):
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.mp_hands = mp.solutions.hands
-
-    def draw_landmark(self,results):
-        if results.multi_hand_landmarks:
-          for hand_landmarks in results.multi_hand_landmarks:
-            self.mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks,
-                self.mp_hands.HAND_CONNECTIONS,
-                self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                self.mp_drawing_styles.get_default_hand_connections_style())
-
-    ##TODO each function return a list of 21 landmarks of x,y,x axis
-
-    def get_x_landmarks(self, results):
-        landmarks = []
-        #checking if landmarks are detected
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(21):
-                    landmarks.append(hand_landmarks.landmark[i].x)
-        else:
-            return landmarks
-
-        return landmarks
-
-    def get_y_landmarks(self, results):
-        landmarks = []
-        # checking if landmarks are detected
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(21):
-                    landmarks.append(hand_landmarks.landmark[i].y)
-        else:
-            return landmarks
-
-        return landmarks
-
-    def get_z_landmarks(self, results):
-        landmarks = []
-        # checking if landmarks are detected
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(21):
-                    landmarks.append(hand_landmarks.landmark[i].z)
-        else:
-            return landmarks
-
-        return landmarks
-
+from CapturedHand import CapturedHand
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     tracker = CapturedHand()
@@ -82,9 +27,16 @@ if __name__ == '__main__':
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        tracker.draw_landmark(results)
+        tracker.draw_landmark(image,results)
 
-        #collecting x,y,z landmarks lists
+        #collecting x,y,z landmarks lists for
+
+        x = tracker.get_x_landmarks(results)
+        y = tracker.get_y_landmarks(results)
+        z = tracker.get_z_landmarks(results)
+
+        tuple = (x,y,z)
+        print(f'tipo:{type(tuple)} \n dati:\n{tuple}')
 
         # Flip the image horizontally for a selfie-view display and show image.
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
